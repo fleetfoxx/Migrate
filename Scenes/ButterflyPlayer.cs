9a6a28842416a331437e1a2c7aeebe6896e60f2f
fleetfoxx <tmporter42+fleetfoxx@gmail.com>
2023-10-08 12:52:48 -0700

    Rotate player instead of planet for performance reasons.

--- Scenes/ButterflyPlayer.cs
+++ Scenes/ButterflyPlayer.cs
@@ -1,23 +1,30 @@
 using Godot;
 using System;
 
-public class ButterflyPlayer : Area
+public class ButterflyPlayer : Spatial
 {
   [Export] private float _speed = 5f;
+  [Export] private float _turnSpeed = 5f;
   [Export] private float _flightAltitude = 104;
   [Export] private float _groundAltitude = 100.1f;
+  [Export] NodePath _playerPath;
   [Export] NodePath _butterflyModelPath;
+  [Export] NodePath _areaPath;
 
   private GlobalSettings _globalSettings;
   private ButterflyModel _butterflyModel;
+  private Spatial _player;
 
   public override void _Ready()
   {
     _globalSettings = GetNode<GlobalSettings>(GlobalSettings.PATH);
     _butterflyModel = GetNode<ButterflyModel>(_butterflyModelPath);
+    _player = GetNode<Spatial>(_playerPath);
 
-    Connect("area_entered", this, nameof(HandleAreaEntered));
-    Connect("area_exited", this, nameof(HandleAreaExited));
+    var area = GetNode<Area>(_areaPath);
+
+    area.Connect("area_entered", this, nameof(HandleAreaEntered));
+    area.Connect("area_exited", this, nameof(HandleAreaExited));
   }
 
   public override void _Process(float delta)
@@ -25,14 +32,22 @@ public class ButterflyPlayer : Area
     if (_globalSettings.IsPaused)
     {
       // fly toward ground
-      var target = new Vector3(GlobalTranslation.x, _groundAltitude, GlobalTranslation.z);
-      GlobalTranslation = GlobalTranslation.MoveToward(target, _speed * delta);
+      var target = new Vector3(_player.Translation.x, _groundAltitude, _player.Translation.z);
+      _player.Translation = _player.Translation.MoveToward(target, _speed * delta);
     }
     else
     {
       // fly toward flight altitude
-      var target = new Vector3(GlobalTranslation.x, _flightAltitude, GlobalTranslation.z);
-      GlobalTranslation = GlobalTranslation.MoveToward(target, _speed * delta);
+      var target = new Vector3(_player.Translation.x, _flightAltitude, _player.Translation.z);
+      _player.Translation = _player.Translation.MoveToward(target, _speed * delta);
+
+
+      RotateObjectLocal(Vector3.Left, Mathf.Deg2Rad(_speed * delta));
+      // RotationDegrees = new Vector3(RotationDegrees.x - _speed * delta, RotationDegrees.y, RotationDegrees.z);
+
+      var axis = Input.GetAxis("turn_left", "turn_right");
+      RotateObjectLocal(Vector3.Down, Mathf.Deg2Rad(axis * _turnSpeed * delta));
+      // RotationDegrees = new Vector3(RotationDegrees.x, RotationDegrees.y - axis * _turnSpeed * delta, RotationDegrees.z);
     }
   }
 
--- Scenes/Planet.cs
+++ Scenes/Planet.cs
@@ -1,48 +1,48 @@
 using Godot;
 using System;
 
-[Tool]
+// [Tool]
 public class Planet : Spatial
 {
-  [Export] private float _speed = 1f;
-  [Export] private float _turnSpeed = 5f;
-  [Export] private bool _spinInEditor = false;
-  [Export] private NodePath _planetMeshPath;
+  // [Export] private float _speed = 1f;
+  // [Export] private float _turnSpeed = 5f;
+  // [Export] private bool _spinInEditor = false;
+  // [Export] private NodePath _planetMeshPath;
 
-  private MeshInstance _planetMesh;
-  private GlobalSettings _globalSettings;
+  // private MeshInstance _planetMesh;
+  // private GlobalSettings _globalSettings;
 
 
-  public override void _Ready()
-  {
-    if (!Engine.EditorHint)
-    {
-      _globalSettings = GetNode<GlobalSettings>(GlobalSettings.PATH);
-    }
+  // public override void _Ready()
+  // {
+  //   if (!Engine.EditorHint)
+  //   {
+  //     _globalSettings = GetNode<GlobalSettings>(GlobalSettings.PATH);
+  //   }
 
-    _planetMesh = GetNode<MeshInstance>(_planetMeshPath);
-    GlobalRotation = Vector3.Zero;
-  }
+  //   _planetMesh = GetNode<MeshInstance>(_planetMeshPath);
+  //   GlobalRotation = Vector3.Zero;
+  // }
 
-  public override void _Process(float delta)
-  {
-    if (Engine.EditorHint)
-    {
-      if (_spinInEditor)
-      {
-        GlobalRotate(Vector3.Right, Mathf.Deg2Rad(_speed * delta));
-      }
-    }
-    else
-    {
-      if (!_globalSettings.IsPaused)
-      {
-        // spin (forward)
-        GlobalRotate(Vector3.Right, Mathf.Deg2Rad(_speed * delta));
+  // public override void _Process(float delta)
+  // {
+  //   if (Engine.EditorHint)
+  //   {
+  //     if (_spinInEditor)
+  //     {
+  //       GlobalRotate(Vector3.Right, Mathf.Deg2Rad(_speed * delta));
+  //     }
+  //   }
+  //   else
+  //   {
+  //     if (!_globalSettings.IsPaused)
+  //     {
+  //       // spin (forward)
+  //       GlobalRotate(Vector3.Right, Mathf.Deg2Rad(_speed * delta));
 
-        var axis = Input.GetAxis("turn_left", "turn_right");
-        GlobalRotate(Vector3.Up, Mathf.Deg2Rad(axis * _turnSpeed * delta));
-      }
-    }
-  }
+  //       var axis = Input.GetAxis("turn_left", "turn_right");
+  //       GlobalRotate(Vector3.Up, Mathf.Deg2Rad(axis * _turnSpeed * delta));
+  //     }
+  //   }
+  // }
 }
